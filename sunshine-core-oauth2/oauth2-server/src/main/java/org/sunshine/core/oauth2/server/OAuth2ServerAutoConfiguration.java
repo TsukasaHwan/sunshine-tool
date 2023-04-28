5b2fb44b80ca58a176635a086403c4957a88f783
Teamo <785415580@qq.com>
2023-04-28 13:30:02 +0800

    feat: 更新oauth2

--- sunshine-core-oauth2/oauth2-server/src/main/java/org/sunshine/core/oauth2/server/OAuth2ServerAutoConfiguration.java
+++ sunshine-core-oauth2/oauth2-server/src/main/java/org/sunshine/core/oauth2/server/OAuth2ServerAutoConfiguration.java
@@ -14,6 +14,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -27,9 +29,11 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.token.*;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.util.Assert;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.sunshine.core.oauth2.server.authorization.OAuth2AuthConsentServiceImpl;
 import org.sunshine.core.oauth2.server.authorization.OAuth2AuthServiceImpl;
 import org.sunshine.core.oauth2.server.authorization.OAuth2ClientRepository;
@@ -37,6 +41,9 @@ import org.sunshine.core.oauth2.server.entity.OAuth2Auth;
 import org.sunshine.core.oauth2.server.entity.OAuth2AuthConsent;
 import org.sunshine.core.oauth2.server.entity.OAuth2Client;
 import org.sunshine.core.oauth2.server.properties.OAuth2ServerProperties;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Teamo
@@ -62,7 +69,8 @@ public class OAuth2ServerAutoConfiguration {
      */
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
-    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http,
+                                                                      CorsConfigurationSource corsConfigurationSource) throws Exception {
         OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
                 new OAuth2AuthorizationServerConfigurer();
 
@@ -77,15 +85,20 @@ public class OAuth2ServerAutoConfiguration {
         http
                 .requestMatcher(endpointsMatcher)
                 .authorizeHttpRequests(authorize ->
-                        authorize.anyRequest().authenticated()
-                )
-                .exceptionHandling(exceptions ->
-                        exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+                        authorize.anyRequest().permitAll()
                 )
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .apply(authorizationServerConfigurer);
 
+        // Turn off csrf protection
         http.csrf().disable();
+
+        // Prevent iframe content from being displayed
+        http.headers().frameOptions().disable();
+
+        // Turn on cross-domain support
+        http.cors().configurationSource(corsConfigurationSource);
+
         return http.build();
     }
 
@@ -203,5 +216,37 @@ public class OAuth2ServerAutoConfiguration {
     @ConditionalOnMissingBean(AuthorizationServerSettings.class)
     public AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder().build();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(CorsConfigurationSource.class)
+    public CorsConfigurationSource corsConfigurationSource() {
+        List<String> allowMethod = Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.HEAD.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.PATCH.name(),
+                HttpMethod.DELETE.name(),
+                HttpMethod.OPTIONS.name()
+        );
+
+        List<String> allowHeader = Arrays.asList(
+                HttpHeaders.ORIGIN,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT,
+                "X-Requested-With"
+        );
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOriginPattern(CorsConfiguration.ALL);
+        corsConfiguration.setAllowedMethods(allowMethod);
+        corsConfiguration.setAllowedHeaders(allowHeader);
+        corsConfiguration.setAllowCredentials(Boolean.TRUE);
+        corsConfiguration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 }
