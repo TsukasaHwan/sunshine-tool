e02e3c12bcbfd3f169e346ee5b2e7f936eadd7c0
Teamo <785415580@qq.com>
2023-05-20 20:01:53 +0800

    feat: 优化util

--- sunshine-core-tool/src/main/java/org/sunshine/core/tool/util/PageUtils.java
+++ sunshine-core-tool/src/main/java/org/sunshine/core/tool/util/PageUtils.java
@@ -140,18 +140,7 @@ public class PageUtils {
      * @param pageSize   每页数
      * @return 总页数
      */
-    public static int totalPage(int totalCount, int pageSize) {
-        return totalPage((long) totalCount, pageSize);
-    }
-
-    /**
-     * 根据总数计算总页数
-     *
-     * @param totalCount 总数
-     * @param pageSize   每页数
-     * @return 总页数
-     */
-    public static int totalPage(long totalCount, int pageSize) {
+    public static int totalPage(long totalCount, long pageSize) {
         if (pageSize == 0) {
             return 0;
         }
