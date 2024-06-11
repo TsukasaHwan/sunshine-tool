84b92ffb84a3bb746e041bee0fa518764962d060
Teamo <785415580@qq.com>
2024-06-11 17:05:41 +0800

    feat: 优化

--- sunshine-core-mybatis/src/main/java/org/sunshine/core/mp/extension/injector/InsertBatchSqlInjector.java
+++ sunshine-core-mybatis/src/main/java/org/sunshine/core/mp/extension/injector/InsertBatchSqlInjector.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.injector.AbstractMethod;
 import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
 import com.baomidou.mybatisplus.core.metadata.TableInfo;
 import com.baomidou.mybatisplus.extension.injector.methods.InsertBatchSomeColumn;
+import org.apache.ibatis.session.Configuration;
 
 import java.util.List;
 
@@ -14,9 +15,9 @@ import java.util.List;
 public class InsertBatchSqlInjector extends DefaultSqlInjector {
 
     @Override
-    public List<AbstractMethod> getMethodList(Class<?> mapperClass, TableInfo tableInfo) {
+    public List<AbstractMethod> getMethodList(Configuration configuration, Class<?> mapperClass, TableInfo tableInfo) {
         // super.getMethodList() 保留 Mybatis Plus 自带的方法
-        List<AbstractMethod> methodList = super.getMethodList(mapperClass, tableInfo);
+        List<AbstractMethod> methodList = super.getMethodList(configuration, mapperClass, tableInfo);
         // 添加自定义方法：批量插入，方法名为 insertBatchSomeColumn
         methodList.add(new InsertBatchSomeColumn());
         return methodList;
