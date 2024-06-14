5cea155736c2b701508b4cfe7da3be1ceab66318
Teamo <785415580@qq.com>
2024-06-14 14:09:39 +0800

    feat: 优化

--- sunshine-core-common/src/main/java/org/sunshine/core/common/config/DefaultWebConfiguration.java
+++ sunshine-core-common/src/main/java/org/sunshine/core/common/config/DefaultWebConfiguration.java
@@ -36,13 +36,13 @@ import java.util.List;
 public class DefaultWebConfiguration implements WebMvcConfigurer {
 
     private final Converter<String, LocalDateTime> localDateTimeConverter = (StringToLocalDateTimeConverter) source ->
-            LocalDateTime.parse(source, DateUtils.DATETIME_FORMATTER);
+            source.isBlank() ? null : LocalDateTime.parse(source, DateUtils.DATETIME_FORMATTER);
 
     private final Converter<String, LocalDate> localDateConverter = (StringToLocalDateConverter) source ->
-            LocalDate.parse(source, DateUtils.DATE_FORMATTER);
+            source.isBlank() ? null : LocalDate.parse(source, DateUtils.DATE_FORMATTER);
 
     private final Converter<String, LocalTime> localTimeConverter = (StringToLocalTimeConverter) source ->
-            LocalTime.parse(source, DateUtils.TIME_FORMATTER);
+            source.isBlank() ? null : LocalTime.parse(source, DateUtils.TIME_FORMATTER);
 
     /**
      * 集成fastJson
