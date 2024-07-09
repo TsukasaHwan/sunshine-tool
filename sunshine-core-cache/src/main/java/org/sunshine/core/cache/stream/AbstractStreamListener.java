294c095a013b028d9be6094e7b6ae58f340d2015
Teamo <785415580@qq.com>
2024-07-09 18:37:33 +0800

    feat: 优化Redis消息队列

--- sunshine-core-cache/src/main/java/org/sunshine/core/cache/stream/AbstractStreamListener.java
+++ sunshine-core-cache/src/main/java/org/sunshine/core/cache/stream/AbstractStreamListener.java
@@ -1,23 +1,15 @@
 package org.sunshine.core.cache.stream;
 
 import com.alibaba.fastjson2.JSON;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Range;
-import org.springframework.data.redis.connection.RedisStreamCommands;
-import org.springframework.data.redis.connection.stream.*;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
 import org.springframework.data.redis.stream.StreamListener;
 import org.sunshine.core.cache.RedisMQTemplate;
+import org.sunshine.core.cache.support.scheduling.DistributedTaskScheduling;
 import org.sunshine.core.tool.util.BeanUtils;
 import org.sunshine.core.tool.util.TypeUtils;
 
 import java.lang.reflect.Type;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author Teamo
@@ -26,7 +18,7 @@ import java.util.Map;
 public abstract class AbstractStreamListener<T extends AbstractStreamMessage>
         implements StreamListener<String, ObjectRecord<String, String>> {
 
-    private final static Logger log = LoggerFactory.getLogger(AbstractStreamListener.class);
+    private static final String LOCK_KEY = "lock:scheduled:redis:trim:%s";
 
     private RedisMQTemplate redisMQTemplate;
 
@@ -55,87 +47,14 @@ public abstract class AbstractStreamListener<T extends AbstractStreamMessage>
     }
 
     /**
-     * 清理消息队列
+     * 清理消息队列（定时任务）
      *
      * @param count 保留数量
      */
-    protected void trimStream(long count) {
-        redisMQTemplate.redisTemplate().opsForStream().trim(streamKey, count);
-    }
-
-    /**
-     * 处理死信
-     */
-    protected void handleDeadLetter() {
-        // 处理死信队列
-        // 获取group中的pending消息信息
-        PendingMessagesSummary pendingMessagesSummary = redisMQTemplate.redisTemplate().opsForStream().pending(streamKey, group);
-        if (pendingMessagesSummary == null) {
-            return;
-        }
-        // 所有pending消息的数量
-        long totalPendingMessages = pendingMessagesSummary.getTotalPendingMessages();
-        if (totalPendingMessages == 0) {
-            return;
-        }
-        // 获取每个消费者的pending消息数量
-        Map<String, Long> pendingMessagesPerConsumer = pendingMessagesSummary.getPendingMessagesPerConsumer();
-        Map<String, List<RecordId>> oldConsumerTransferMessageMap = new HashMap<>();
-        // 遍历每个消费者中的pending消息
-        pendingMessagesPerConsumer.forEach((consumer, consumerTotalPendingMessages) -> {
-            // 待重试的 RecordId
-            if (consumerTotalPendingMessages > 0) {
-                // 读取消费者pending队列的前10条记录，从ID=0的记录开始，一直到ID最大值
-                PendingMessages pendingMessages = redisMQTemplate.redisTemplate().opsForStream().pending(streamKey, Consumer.from(group, consumer), Range.closed("0", "+"), 10);
-                // 遍历所有pending消息的详情
-                List<RecordId> transferGroupMessageList = new ArrayList<>(pendingMessages.size());
-                pendingMessages.forEach(pendingMessage -> handleTransferGroupList(pendingMessage, transferGroupMessageList));
-                if (transferGroupMessageList.size() > 0) {
-                    oldConsumerTransferMessageMap.put(consumer, transferGroupMessageList);
-                }
-            }
-        });
-
-        changeConsumer(oldConsumerTransferMessageMap);
-    }
-
-    /**
-     * 处理待转移消息列表
-     *
-     * @param pendingMessage           挂起的消息
-     * @param transferGroupMessageList 待转移的RecordId
-     */
-    protected void handleTransferGroupList(PendingMessage pendingMessage, List<RecordId> transferGroupMessageList) {
-        // 消息的ID
-        RecordId recordId = pendingMessage.getId();
-        // 消息从消费组中获取，到此刻的时间
-        Duration elapsedTimeSinceLastDelivery = pendingMessage.getElapsedTimeSinceLastDelivery();
-        // 消息被获取的次数
-        long deliveryCount = pendingMessage.getTotalDeliveryCount();
-        if (elapsedTimeSinceLastDelivery.getSeconds() > 20 && deliveryCount == 1) {
-            transferGroupMessageList.add(recordId);
-        } else {
-            redisMQTemplate.redisTemplate().opsForStream().acknowledge(streamKey, group, recordId);
-        }
-    }
-
-    /**
-     * 改变消费者
-     *
-     * @param oldConsumerTransferMessageMap 旧消费者待转移消息Map
-     */
-    protected void changeConsumer(Map<String, List<RecordId>> oldConsumerTransferMessageMap) {
-        oldConsumerTransferMessageMap.forEach((oldConsumer, recordIds) -> {
-            // 根据当前consumer去获取另外一个consumer
-            StreamInfo.XInfoConsumers consumers = redisMQTemplate.redisTemplate().opsForStream().consumers(streamKey, group);
-            List<StreamInfo.XInfoConsumer> newConsumers = consumers.stream().filter(consumer -> !consumer.consumerName().equals(oldConsumer)).toList();
-            newConsumers.forEach(consumer -> {
-                List<MapRecord<String, Object, Object>> mapRecords = redisMQTemplate.redisTemplate().opsForStream().claim(streamKey, group, oldConsumer, RedisStreamCommands.XClaimOptions.minIdle(Duration.ofSeconds(10)).ids(recordIds));
-                if (log.isDebugEnabled()) {
-                    mapRecords.forEach(entries -> log.debug("转移消息 id: {}, value: {}", entries.getId(), entries.getValue()));
-                }
-            });
-        });
+    protected void trim(long count) {
+        DistributedTaskScheduling scheduling = () -> redisMQTemplate.redisTemplate().opsForStream().trim(streamKey, count);
+        String key = String.format(LOCK_KEY, streamKey);
+        scheduling.execute(key);
     }
 
     @SuppressWarnings("unchecked")
---
+++ sunshine-core-cache/src/main/java/org/sunshine/core/cache/stream/RedisPendingMessageScheduledTask.java
@@ -0,0 +1,116 @@
+package org.sunshine.core.cache.stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Range;
+import org.springframework.data.redis.connection.RedisStreamCommands;
+import org.springframework.data.redis.connection.stream.Record;
+import org.springframework.data.redis.connection.stream.*;
+import org.springframework.data.redis.core.StreamOperations;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.sunshine.core.cache.RedisMQTemplate;
+import org.sunshine.core.cache.support.scheduling.DistributedTaskScheduling;
+import org.sunshine.core.tool.util.CollectionUtils;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Teamo
+ * @since 2024/7/9
+ */
+public class RedisPendingMessageScheduledTask {
+
+    private final static Logger log = LoggerFactory.getLogger(RedisPendingMessageScheduledTask.class);
+
+    private final static String LOCK_KEY = "lock:scheduled:redis:pending:message";
+
+    private final List<AbstractStreamListener<?>> listeners;
+
+    private final RedisMQTemplate redisMQTemplate;
+
+    public RedisPendingMessageScheduledTask(List<AbstractStreamListener<?>> listeners, RedisMQTemplate redisMQTemplate) {
+        this.listeners = listeners;
+        this.redisMQTemplate = redisMQTemplate;
+    }
+
+    @Scheduled(cron = "30 * * * * ?")
+    public void run() {
+        DistributedTaskScheduling scheduling = this::handleDeadLetter;
+        scheduling.execute(LOCK_KEY);
+    }
+
+    /**
+     * 处理死信
+     */
+    private void handleDeadLetter() {
+        StreamOperations<String, Object, Object> ops = redisMQTemplate.redisTemplate().opsForStream();
+        listeners.forEach(listener -> {
+            String streamKey = listener.getStreamKey();
+            String group = listener.getGroup();
+            // 处理死信队列
+            // 获取group中的pending消息信息
+            PendingMessagesSummary pendingMessagesSummary = ops.pending(streamKey, group);
+            if (pendingMessagesSummary == null) {
+                return;
+            }
+            // 所有pending消息的数量
+            long totalPendingMessages = pendingMessagesSummary.getTotalPendingMessages();
+            if (totalPendingMessages == 0) {
+                return;
+            }
+            // 获取每个消费者的pending消息数量
+            Map<String, Long> pendingMessagesPerConsumer = pendingMessagesSummary.getPendingMessagesPerConsumer();
+            Map<String, List<RecordId>> oldConsumerTransferMessageMap = new HashMap<>();
+            // 遍历每个消费者中的pending消息
+            pendingMessagesPerConsumer.forEach((consumer, consumerTotalPendingMessages) -> {
+                // 待重试的 RecordId
+                if (consumerTotalPendingMessages == 0) {
+                    return;
+                }
+                // 读取消费者pending队列
+                PendingMessages pendingMessages = ops.pending(streamKey, Consumer.from(group, consumer));
+                List<RecordId> transferGroupMessageList = new ArrayList<>(pendingMessages.size());
+                // 遍历所有pending消息的详情
+                pendingMessages.forEach(pendingMessage -> {
+                    // 消息的ID
+                    RecordId recordId = pendingMessage.getId();
+                    // 消息从消费组中获取，到此刻的时间
+                    Duration elapsedTimeSinceLastDelivery = pendingMessage.getElapsedTimeSinceLastDelivery();
+                    // 消息被获取的次数
+                    long deliveryCount = pendingMessage.getTotalDeliveryCount();
+                    if (elapsedTimeSinceLastDelivery.getSeconds() > 20 && deliveryCount == 1) {
+                        // 转组
+                        transferGroupMessageList.add(recordId);
+                    } else if (deliveryCount > 1) {
+                        // 重新投递并消费
+                        List<MapRecord<String, Object, Object>> records = ops.range(streamKey, Range.rightOpen(recordId.getValue(), recordId.getValue()));
+                        if (CollectionUtils.isEmpty(records)) {
+                            return;
+                        }
+                        MapRecord<String, Object, Object> entries = records.get(0);
+                        ops.add(Record.of(entries.getValue()).withStreamKey(streamKey));
+                        ops.acknowledge(streamKey, group, recordId);
+                    }
+                });
+
+                if (transferGroupMessageList.size() > 0) {
+                    oldConsumerTransferMessageMap.put(consumer, transferGroupMessageList);
+                }
+            });
+
+            oldConsumerTransferMessageMap.forEach((oldConsumer, recordIds) -> {
+                // 根据当前consumer去获取另外一个consumer
+                StreamInfo.XInfoConsumers consumers = ops.consumers(streamKey, group);
+                List<StreamInfo.XInfoConsumer> newConsumers = consumers.stream().filter(consumer -> !consumer.consumerName().equals(oldConsumer)).toList();
+                newConsumers.forEach(consumer -> {
+                    List<MapRecord<String, Object, Object>> mapRecords = ops.claim(streamKey, group, oldConsumer, RedisStreamCommands.XClaimOptions.minIdle(Duration.ofSeconds(10)).ids(recordIds));
+                    mapRecords.forEach(entries -> log.info("转移消息 id: {}, value: {}", entries.getId(), entries.getValue()));
+                });
+            });
+        });
+    }
+}
