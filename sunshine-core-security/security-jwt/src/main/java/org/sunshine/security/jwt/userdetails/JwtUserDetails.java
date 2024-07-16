73b18398904a82fc4653b9f4fd1f23fc59efc449
Teamo <785415580@qq.com>
2024-07-16 20:51:09 +0800

    feat: 优化

--- sunshine-core-security/security-jwt/src/main/java/org/sunshine/security/jwt/userdetails/JwtUserDetails.java
+++ sunshine-core-security/security-jwt/src/main/java/org/sunshine/security/jwt/userdetails/JwtUserDetails.java
@@ -1,5 +1,7 @@
 package org.sunshine.security.jwt.userdetails;
 
+import com.alibaba.fastjson2.annotation.JSONField;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.security.core.userdetails.UserDetails;
 
 /**
@@ -9,21 +11,29 @@ import org.springframework.security.core.userdetails.UserDetails;
 public abstract class JwtUserDetails implements UserDetails {
 
     @Override
+    @Schema(hidden = true)
+    @JSONField(serialize = false)
     public boolean isAccountNonExpired() {
         return true;
     }
 
     @Override
+    @Schema(hidden = true)
+    @JSONField(serialize = false)
     public boolean isAccountNonLocked() {
         return true;
     }
 
     @Override
+    @Schema(hidden = true)
+    @JSONField(serialize = false)
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
     @Override
+    @Schema(hidden = true)
+    @JSONField(serialize = false)
     public boolean isEnabled() {
         return true;
     }
