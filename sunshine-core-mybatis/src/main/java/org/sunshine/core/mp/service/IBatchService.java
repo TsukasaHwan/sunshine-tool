758c9cb74b8a7e7b87db03b039b0dfe31e79a5f9
Teamo <785415580@qq.com>
2025-02-11 17:44:35 +0800

    feat: mybatis plus 更新

--- sunshine-core-mybatis/src/main/java/org/sunshine/core/mp/service/IBatchService.java
+++ sunshine-core-mybatis/src/main/java/org/sunshine/core/mp/service/IBatchService.java
@@ -1,11 +1,12 @@
 package org.sunshine.core.mp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.sunshine.core.mp.repository.IBatchRepository;
 
 /**
  * @author Teamo
  * @since 2023/5/6
  */
-public interface IBatchService<T> extends IService<T> {
+public interface IBatchService<T> extends IService<T>, IBatchRepository<T> {
 
 }
