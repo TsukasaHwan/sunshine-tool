419611622e609689db09b22ee07cb9f9f7c9a922
Teamo <785415580@qq.com>
2025-03-15 20:36:34 +0800

    feat: 架构优化

--- sunshine-core-cache/src/main/java/org/sunshine/core/cache/stream/StreamDeadLetterQueueProcessor.java
+++ sunshine-core-cache/src/main/java/org/sunshine/core/cache/stream/StreamDeadLetterQueueProcessor.java
@@ -115,7 +115,7 @@ public class StreamDeadLetterQueueProcessor {
 
                 List<MapRecord<String, Object, Object>> records = streamOperations.range(streamKey, Range.just(recordId.getValue()));
                 if (CollectionUtils.isEmpty(records)) {
-                    logger.warn("[RECORD_NOT_FOUND] 未找到消息记录{}，流键：{}", recordId, streamKey);
+                    logger.warn("[RECORD_NOT_FOUND] 未找到消息记录{}自动确认，流键：{}", recordId, streamKey);
                     // 自动确认
                     streamOperations.acknowledge(streamKey, group, recordId);
                     return;
