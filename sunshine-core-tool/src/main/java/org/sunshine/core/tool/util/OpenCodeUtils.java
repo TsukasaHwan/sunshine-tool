cfdb487ee16b7c385c8e76f8a14e6de46b9dd7cf
Teamo <785415580@qq.com>
2025-03-26 19:08:12 +0800

    feat: 优化

--- sunshine-core-tool/src/main/java/org/sunshine/core/tool/util/OpenCodeUtils.java
+++ sunshine-core-tool/src/main/java/org/sunshine/core/tool/util/OpenCodeUtils.java
@@ -8,12 +8,12 @@ import java.security.MessageDigest;
  */
 public class OpenCodeUtils {
 
-    private final static String[] CHARS = new String[]{"a", "b", "c", "d", "e", "f",
-            "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-            "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5",
-            "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
-            "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
-            "W", "X", "Y", "Z"};
+    private final static char[] CHARS = new char[]{'a', 'b', 'c', 'd', 'e', 'f',
+            'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5',
+            '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+            'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+            'W', 'X', 'Y', 'Z'};
 
     /**
      * 生成appId
