abe42f03ffaed11a4d5e5415c8e64f22c1e9abb6
Teamo <785415580@qq.com>
2025-04-07 20:57:54 +0800

    feat: 采用策略模式加工厂模式优化认证逻辑，方便后续扩展

--- sunshine-core-security/security-jwt/src/main/java/org/sunshine/security/jwt/authenticator/AbstractTokenAuthenticator.java
+++ sunshine-core-security/security-jwt/src/main/java/org/sunshine/security/jwt/authenticator/AbstractTokenAuthenticator.java
@@ -45,6 +45,8 @@ public abstract class AbstractTokenAuthenticator implements InitializingBean, Ap
 
     private static PathPatternRequestMatcher sharedRefreshTokenMatcher;
 
+    private static boolean init = false;
+
     protected AbstractTokenAuthenticator(JwtSecurityProperties jwtSecurityProperties, UserDetailsService userDetailsService) {
         this.jwtSecurityProperties = jwtSecurityProperties;
         this.userDetailsService = userDetailsService;
@@ -56,33 +58,39 @@ public abstract class AbstractTokenAuthenticator implements InitializingBean, Ap
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        if (init) {
+            return;
+        }
+
         if (jwtSecurityProperties.getEnabledRefreshTokenApiAnnotation()) {
-            if (sharedRefreshTokenMatcher == null) {
-                RequestMappingHandlerMapping handlerMapping = context.getBean(RequestMappingHandlerMapping.class);
-                List<RequestMappingInfo> refreshTokenApiMapping = handlerMapping.getHandlerMethods().entrySet()
-                        .stream()
-                        .filter(entry -> Objects.nonNull(ClassUtils.getAnnotation(entry.getValue(), RefreshTokenApi.class)))
-                        .map(Map.Entry::getKey)
-                        .toList();
-
-                Assert.state(refreshTokenApiMapping.size() < 2, "The @RefreshTokenApi annotation can only be used once on method");
-
-                if (refreshTokenApiMapping.isEmpty()) {
-                    return;
-                }
-                RequestMappingInfo requestMappingInfo = refreshTokenApiMapping.get(0);
-                String path = extractPathFromRequestMapping(requestMappingInfo);
-                RequestMethodsRequestCondition methodsCondition = requestMappingInfo.getMethodsCondition();
-                Optional<RequestMethod> requestMethodOpt = methodsCondition.getMethods().stream().findFirst();
-                requestMethodOpt.ifPresentOrElse(
-                        requestMethod -> sharedRefreshTokenMatcher = createPathMatcher(requestMethod.asHttpMethod(), path),
-                        () -> sharedRefreshTokenMatcher = createPathMatcher(null, path)
-                );
+            if (sharedRefreshTokenMatcher != null) {
+                return;
             }
+            RequestMappingHandlerMapping handlerMapping = context.getBean(RequestMappingHandlerMapping.class);
+            List<RequestMappingInfo> refreshTokenApiMapping = handlerMapping.getHandlerMethods().entrySet()
+                    .stream()
+                    .filter(entry -> Objects.nonNull(ClassUtils.getAnnotation(entry.getValue(), RefreshTokenApi.class)))
+                    .map(Map.Entry::getKey)
+                    .toList();
+
+            Assert.state(refreshTokenApiMapping.size() < 2, "The @RefreshTokenApi annotation can only be used once on method");
+
+            if (refreshTokenApiMapping.isEmpty()) {
+                return;
+            }
+            RequestMappingInfo requestMappingInfo = refreshTokenApiMapping.get(0);
+            String path = extractPathFromRequestMapping(requestMappingInfo);
+            RequestMethodsRequestCondition methodsCondition = requestMappingInfo.getMethodsCondition();
+            Optional<RequestMethod> requestMethodOpt = methodsCondition.getMethods().stream().findFirst();
+            requestMethodOpt.ifPresentOrElse(
+                    requestMethod -> sharedRefreshTokenMatcher = createPathMatcher(requestMethod.asHttpMethod(), path),
+                    () -> sharedRefreshTokenMatcher = createPathMatcher(null, path)
+            );
         } else {
             sharedRefreshTokenMatcher = createPathMatcher(null, jwtSecurityProperties.getRefreshTokenPath());
         }
 
+        init = true;
     }
 
     @Override
