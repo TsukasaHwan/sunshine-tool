ca64f4de42b32c0ea902903b6e98d1fa0d49b960
Teamo <785415580@qq.com>
2025-04-14 22:06:38 +0800

    feat: 优化

--- sunshine-core-tool/src/main/java/org/sunshine/core/tool/util/InviteCodeUtils.java
+++ sunshine-core-tool/src/main/java/org/sunshine/core/tool/util/InviteCodeUtils.java
@@ -1,6 +1,8 @@
 package org.sunshine.core.tool.util;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 邀请码生成util
@@ -21,7 +23,7 @@ public class InviteCodeUtils {
     /**
      * 邀请码长度
      */
-    private final int codeLength = 6;
+    private static final int CODE_LENGTH = 6;
 
     /**
      * PRIME1 与 CHARS 的长度 L互质，可保证 ( id * PRIME1) % L 在 [0,L)上均匀分布
@@ -35,20 +37,31 @@ public class InviteCodeUtils {
     private final static int PRIME2 = 11;
 
     /**
+     * 字符串与数字的对应关系
+     */
+    private static final Map<Character, Integer> CHAR_TO_INDEX = new HashMap<>(43);
+
+    static {
+        for (int i = 0; i < CHARS_LENGTH; i++) {
+            CHAR_TO_INDEX.put(CHARS[i], i);
+        }
+    }
+
+    /**
      * 生成邀请码
      *
      * @param id   唯一的id主键
-     * @param salt 盐
+     * @param salt 盐（正整数）
      * @return code
      */
-    public String gen(Long id, long salt) {
+    public static String gen(Long id, long salt) {
         // 补位
-        id = id * PRIME1 + salt;
+        long adjustedId = id * PRIME1 + salt;
         // 将 id 转换成32进制的值
-        long[] b = new long[codeLength];
+        long[] b = new long[CODE_LENGTH];
         // 32进制数
-        b[0] = id;
-        for (int i = 0; i < codeLength - 1; i++) {
+        b[0] = adjustedId;
+        for (int i = 0; i < CODE_LENGTH - 1; i++) {
             b[i + 1] = b[i] / CHARS_LENGTH;
             // 按位扩散
             b[i] = (b[i] + i * b[0]) % CHARS_LENGTH;
@@ -56,9 +69,9 @@ public class InviteCodeUtils {
         b[5] = (b[0] + b[1] + b[2] + b[3] + b[4]) * PRIME1 % CHARS_LENGTH;
 
         // 进行混淆
-        long[] codeIndexArray = new long[codeLength];
-        for (int i = 0; i < codeLength; i++) {
-            codeIndexArray[i] = b[i * PRIME2 % codeLength];
+        long[] codeIndexArray = new long[CODE_LENGTH];
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            codeIndexArray[i] = b[i * PRIME2 % CODE_LENGTH];
         }
 
         StringBuilder buffer = new StringBuilder();
@@ -73,47 +86,33 @@ public class InviteCodeUtils {
      * @param salt 盐
      * @return id
      */
-    public Long decode(String code, long salt) {
-        if (code.length() != codeLength) {
+    public static Long decode(String code, long salt) {
+        if (code.length() != CODE_LENGTH) {
             return null;
         }
         // 将字符还原成对应数字
-        long[] a = new long[codeLength];
-        for (int i = 0; i < codeLength; i++) {
+        long[] a = new long[CODE_LENGTH];
+        for (int i = 0; i < CODE_LENGTH; i++) {
             char c = code.charAt(i);
-            int index = findIndex(c);
-            if (index == -1) {
+            Integer index = CHAR_TO_INDEX.get(c);
+            if (index == null) {
                 // 异常字符串
                 return null;
             }
-            a[i * PRIME2 % codeLength] = index;
+            a[i * PRIME2 % CODE_LENGTH] = index;
         }
 
-        long[] b = new long[codeLength];
-        for (int i = codeLength - 2; i >= 0; i--) {
+        long[] b = new long[CODE_LENGTH];
+        for (int i = CODE_LENGTH - 2; i >= 0; i--) {
             b[i] = (a[i] - a[0] * i + (long) CHARS_LENGTH * i) % CHARS_LENGTH;
         }
 
         long res = 0;
-        for (int i = codeLength - 2; i >= 0; i--) {
+        for (int i = CODE_LENGTH - 2; i >= 0; i--) {
             res += b[i];
             res *= (i > 0 ? CHARS_LENGTH : 1);
         }
         return (res - salt) / PRIME1;
     }
 
-    /**
-     * 查找对应字符的index
-     *
-     * @param c 字符
-     * @return index
-     */
-    private static int findIndex(char c) {
-        for (int i = 0; i < CHARS_LENGTH; i++) {
-            if (CHARS[i] == c) {
-                return i;
-            }
-        }
-        return -1;
-    }
 }
