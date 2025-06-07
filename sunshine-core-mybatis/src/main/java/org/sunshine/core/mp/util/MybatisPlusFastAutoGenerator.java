dc801f1aaed6407c7423fa0429711117a31e3dbd
Teamo <785415580@qq.com>
2025-06-07 19:07:04 +0800

    feat: 更新jar

--- sunshine-core-mybatis/src/main/java/org/sunshine/core/mp/util/MybatisPlusFastAutoGenerator.java
+++ sunshine-core-mybatis/src/main/java/org/sunshine/core/mp/util/MybatisPlusFastAutoGenerator.java
@@ -44,7 +44,7 @@ public class MybatisPlusFastAutoGenerator {
                             .enableLombok()
                             .disableSerialVersionUID()
                             .addTableFills(new Column(scanner.apply("创建时间字段名："), FieldFill.INSERT))
-                            .addTableFills(new Column(scanner.apply("修改时间字段名："), FieldFill.INSERT_UPDATE));
+                            .addTableFills(new Column(scanner.apply("修改时间字段名："), FieldFill.UPDATE));
 
                     String logicDeleteColumnName = scanner.apply("逻辑删除字段名：");
                     entityBuilder.addTableFills(new Column(logicDeleteColumnName, FieldFill.INSERT));
