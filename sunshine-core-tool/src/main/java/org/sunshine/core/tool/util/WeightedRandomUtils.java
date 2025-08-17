acb589c1a7092005db81db401c59d9566e5728fc
Teamo <785415580@qq.com>
2025-08-18 00:28:36 +0800

    perf(sunshine-core-tool):优化加权随机算法的性能- 将 max(Integer::compare) 替换为 max()，简化代码逻辑 - 使用 mapToInt而不是 map，避免自动装箱带来的性能开销

--- sunshine-core-tool/src/main/java/org/sunshine/core/tool/util/WeightedRandomUtils.java
+++ sunshine-core-tool/src/main/java/org/sunshine/core/tool/util/WeightedRandomUtils.java
@@ -24,7 +24,8 @@ public class WeightedRandomUtils {
 
         BigDecimal totalWeight = weights.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
         int scale = weights.stream()
-                .map(weight -> Math.max(0, weight.stripTrailingZeros().scale())).max(Integer::compare)
+                .mapToInt(weight -> Math.max(0, weight.stripTrailingZeros().scale()))
+                .max()
                 .orElse(0);
         BigDecimal multiplier = BigDecimal.TEN.pow(scale);
         long randomValue = ThreadLocalRandom.current().nextLong(totalWeight.multiply(multiplier).longValueExact());
