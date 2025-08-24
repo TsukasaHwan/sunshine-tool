fb92c89c2c25162bc5291a8677b3b3366afd661d
Teamo <785415580@qq.com>
2025-08-24 13:04:07 +0800

    refactor(sunshine-core-cloud): 优化 OkHttp3ClientHttpRequest 类的代码结构
    - 调整类和方法的缩进，使用空格替代制表符- 移除未使用的 imports，简化代码
    - 更新代码注释，保持一致性

--- sunshine-core-cloud/src/main/java/org/sunshine/core/cloud/http/client/OkHttp3ClientHttpRequest.java
+++ sunshine-core-cloud/src/main/java/org/sunshine/core/cloud/http/client/OkHttp3ClientHttpRequest.java
@@ -43,95 +43,94 @@ import java.net.URI;
  */
 public class OkHttp3ClientHttpRequest extends AbstractStreamingClientHttpRequest {
 
-	private final OkHttpClient client;
+    private final OkHttpClient client;
 
-	private final URI uri;
+    private final URI uri;
 
-	private final HttpMethod method;
-
-
-	public OkHttp3ClientHttpRequest(OkHttpClient client, URI uri, HttpMethod method) {
-		this.client = client;
-		this.uri = uri;
-		this.method = method;
-	}
+    private final HttpMethod method;
 
 
-	@Override
-	public HttpMethod getMethod() {
-		return this.method;
-	}
-
-	@Override
-	public URI getURI() {
-		return this.uri;
-	}
-
-	@Override
-	@SuppressWarnings("removal")
-	protected ClientHttpResponse executeInternal(HttpHeaders headers, @Nullable Body body) throws IOException {
-
-		RequestBody requestBody;
-		if (body != null) {
-			requestBody = new BodyRequestBody(headers, body);
-		} else if (okhttp3.internal.http.HttpMethod.requiresRequestBody(getMethod().name())) {
-			String header = headers.getFirst(HttpHeaders.CONTENT_TYPE);
-			MediaType contentType = (header != null) ? MediaType.parse(header) : null;
-			requestBody = RequestBody.create(contentType, new byte[0]);
-		} else {
-			requestBody = null;
-		}
-		Request.Builder builder = new Request.Builder()
-			.url(this.uri.toURL());
-		builder.method(this.method.name(), requestBody);
-		headers.forEach((headerName, headerValues) -> {
-			for (String headerValue : headerValues) {
-				builder.addHeader(headerName, headerValue);
-			}
-		});
-		Request request = builder.build();
-		return new OkHttp3ClientHttpResponse(this.client.newCall(request).execute());
-	}
+    public OkHttp3ClientHttpRequest(OkHttpClient client, URI uri, HttpMethod method) {
+        this.client = client;
+        this.uri = uri;
+        this.method = method;
+    }
 
 
-	private static class BodyRequestBody extends RequestBody {
+    @Override
+    public HttpMethod getMethod() {
+        return this.method;
+    }
 
-		private final HttpHeaders headers;
+    @Override
+    public URI getURI() {
+        return this.uri;
+    }
 
-		private final Body body;
+    @Override
+    protected ClientHttpResponse executeInternal(HttpHeaders headers, Body body) throws IOException {
+
+        RequestBody requestBody;
+        if (body != null) {
+            requestBody = new BodyRequestBody(headers, body);
+        } else if (okhttp3.internal.http.HttpMethod.requiresRequestBody(getMethod().name())) {
+            String header = headers.getFirst(HttpHeaders.CONTENT_TYPE);
+            MediaType contentType = (header != null) ? MediaType.parse(header) : null;
+            requestBody = RequestBody.create(contentType, new byte[0]);
+        } else {
+            requestBody = null;
+        }
+        Request.Builder builder = new Request.Builder()
+                .url(this.uri.toURL());
+        builder.method(this.method.name(), requestBody);
+        headers.forEach((headerName, headerValues) -> {
+            for (String headerValue : headerValues) {
+                builder.addHeader(headerName, headerValue);
+            }
+        });
+        Request request = builder.build();
+        return new OkHttp3ClientHttpResponse(this.client.newCall(request).execute());
+    }
 
 
-		public BodyRequestBody(HttpHeaders headers, Body body) {
-			this.headers = headers;
-			this.body = body;
-		}
+    private static class BodyRequestBody extends RequestBody {
 
-		@Override
-		public long contentLength() {
-			return this.headers.getContentLength();
-		}
+        private final HttpHeaders headers;
 
-		@Nullable
-		@Override
-		public MediaType contentType() {
-			String contentType = this.headers.getFirst(HttpHeaders.CONTENT_TYPE);
-			if (StringUtils.hasText(contentType)) {
-				return MediaType.parse(contentType);
-			} else {
-				return null;
-			}
-		}
+        private final Body body;
 
-		@Override
-		public void writeTo(BufferedSink sink) throws IOException {
-			this.body.writeTo(sink.outputStream());
-		}
 
-		@Override
-		public boolean isOneShot() {
-			return !this.body.repeatable();
-		}
-	}
+        public BodyRequestBody(HttpHeaders headers, Body body) {
+            this.headers = headers;
+            this.body = body;
+        }
+
+        @Override
+        public long contentLength() {
+            return this.headers.getContentLength();
+        }
+
+        @Nullable
+        @Override
+        public MediaType contentType() {
+            String contentType = this.headers.getFirst(HttpHeaders.CONTENT_TYPE);
+            if (StringUtils.hasText(contentType)) {
+                return MediaType.parse(contentType);
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        public void writeTo(BufferedSink sink) throws IOException {
+            this.body.writeTo(sink.outputStream());
+        }
+
+        @Override
+        public boolean isOneShot() {
+            return !this.body.repeatable();
+        }
+    }
 
 
 }
