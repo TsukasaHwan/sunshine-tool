7e5c913feba600071a5c50ed896daafc40d21536
Teamo <785415580@qq.com>
2025-08-24 13:07:45 +0800

    style(sunshine-core-cloud):格式化 AbstractStreamingClientHttpRequest 类的代码
    - 调整了代码的缩进和空格
    - 使代码风格更加一致和可读

--- sunshine-core-cloud/src/main/java/org/sunshine/core/cloud/http/client/AbstractStreamingClientHttpRequest.java
+++ sunshine-core-cloud/src/main/java/org/sunshine/core/cloud/http/client/AbstractStreamingClientHttpRequest.java
@@ -36,51 +36,51 @@ import java.io.OutputStream;
  * @since 6.1
  */
 public abstract class AbstractStreamingClientHttpRequest extends AbstractClientHttpRequest
-	implements StreamingHttpOutputMessage {
+        implements StreamingHttpOutputMessage {
 
-	@Nullable
-	private Body body;
+    @Nullable
+    private Body body;
 
-	@Nullable
-	private FastByteArrayOutputStream bodyStream;
-
-
-	@Override
-	protected final OutputStream getBodyInternal(HttpHeaders headers) {
-		Assert.state(this.body == null, "Invoke either getBody or setBody; not both");
-
-		if (this.bodyStream == null) {
-			this.bodyStream = new FastByteArrayOutputStream(1024);
-		}
-		return this.bodyStream;
-	}
-
-	@Override
-	public final void setBody(Body body) {
-		Assert.notNull(body, "Body must not be null");
-		assertNotExecuted();
-		Assert.state(this.bodyStream == null, "Invoke either getBody or setBody; not both");
-
-		this.body = body;
-	}
-
-	@Override
-	protected final ClientHttpResponse executeInternal(HttpHeaders headers) throws IOException {
-		if (this.body == null && this.bodyStream != null) {
-			this.body = outputStream -> this.bodyStream.writeTo(outputStream);
-		}
-		return executeInternal(headers, this.body);
-	}
+    @Nullable
+    private FastByteArrayOutputStream bodyStream;
 
 
-	/**
-	 * Abstract template method that writes the given headers and content to the HTTP request.
-	 *
-	 * @param headers the HTTP headers
-	 * @param body    the HTTP body, may be {@code null} if no body was {@linkplain #setBody(Body) set}
-	 * @return the response object for the executed request
-	 * @since 6.1
-	 */
-	protected abstract ClientHttpResponse executeInternal(HttpHeaders headers, @Nullable Body body) throws IOException;
+    @Override
+    protected final OutputStream getBodyInternal(HttpHeaders headers) {
+        Assert.state(this.body == null, "Invoke either getBody or setBody; not both");
+
+        if (this.bodyStream == null) {
+            this.bodyStream = new FastByteArrayOutputStream(1024);
+        }
+        return this.bodyStream;
+    }
+
+    @Override
+    public final void setBody(Body body) {
+        Assert.notNull(body, "Body must not be null");
+        assertNotExecuted();
+        Assert.state(this.bodyStream == null, "Invoke either getBody or setBody; not both");
+
+        this.body = body;
+    }
+
+    @Override
+    protected final ClientHttpResponse executeInternal(HttpHeaders headers) throws IOException {
+        if (this.body == null && this.bodyStream != null) {
+            this.body = outputStream -> this.bodyStream.writeTo(outputStream);
+        }
+        return executeInternal(headers, this.body);
+    }
+
+
+    /**
+     * Abstract template method that writes the given headers and content to the HTTP request.
+     *
+     * @param headers the HTTP headers
+     * @param body    the HTTP body, may be {@code null} if no body was {@linkplain #setBody(Body) set}
+     * @return the response object for the executed request
+     * @since 6.1
+     */
+    protected abstract ClientHttpResponse executeInternal(HttpHeaders headers, Body body) throws IOException;
 
 }
